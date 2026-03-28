62aec817f84ec8d9d785d86de4a739eb97051450
Jonathan-Harris1975 <johnbristol26@gmail.com>
2026-03-28 05:00:38 +0000

    Add files via upload

--- functions/404.js
+++ functions/404.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const headers = new Headers(assetResponse.headers);
   return new Response(assetResponse.body, {
     status: 404,
-    statusText: 'Not Found',
+    statusText: "Not Found",
     headers,
   });
 }
