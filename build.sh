66dd79fdf3cdc2b9f0f75481211392f42a0c23fe
Jonathan-Harris1975 <johnbristol26@gmail.com>
2026-03-28 04:44:44 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ REPO_ROOT="$(cd "$(dirname "$0")" && pwd)"
 cd "$REPO_ROOT"
 
 python3 -m pip install --disable-pip-version-check --quiet -r requirements.txt
-python3 scripts/deployment_ci.py
+python3 scripts/deployment_ci.py "$@"
