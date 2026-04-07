f4252a13256b63a65809ee5005184d3ca2567c39
Jonathan-Harris1975 <johnbristol26@gmail.com>
2026-04-07 22:37:05 +0100

    Delete functions/404.js

--- functions/404.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const assetResponse = await context.next();
-  const headers = new Headers(assetResponse.headers);
-  return new Response(assetResponse.body, {
-    status: 404,
-    statusText: "Not Found",
-    headers,
-  });
-}
