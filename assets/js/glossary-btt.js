68387f54d76b2d2f125d894b0961247882978d25
Jonathan-Harris1975 <johnbristol26@gmail.com>
2026-04-07 22:29:05 +0100

    Delete assets/js/glossary-btt.js

--- assets/js/glossary-btt.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  var btn = document.getElementById('bttBtn');
-  if(!btn) return;
-  window.addEventListener('scroll', function(){
-    btn.classList.toggle('visible', window.scrollY > 400);
-  }, {passive:true});
-})();
