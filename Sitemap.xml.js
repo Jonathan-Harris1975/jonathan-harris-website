45610b71d954cf06eadb31bd03c4cc50edb50e13
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-07-26 09:54:36 +0100

    Delete Sitemap.xml.js

--- Sitemap.xml.js
+++
@@ -1,8 +0,0 @@
-const CANONICAL_PATH = "/sitemap.xml";
-
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.pathname = CANONICAL_PATH;
-  url.search = "";
-  return Response.redirect(url.toString(), 301);
-}
