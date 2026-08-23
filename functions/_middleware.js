d1d260c9f6c030a7e9914d134c9e0440eb1261af
Jonathan-Harris1975 <205491236+Jonathan-Harris1975@users.noreply.github.com>
2026-08-23 21:31:55 +0100

    Add files via upload

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,30 @@
 import { withFunctionSecurityHeaders } from "./_shared/security.js";
 
+const SUPPORT_ALIASES = new Map([
+  ["/robot.txt", "/robots.txt"],
+  ["/Sitemap.xml", "/sitemap.xml"],
+  ["/site-map.xml", "/sitemap.xml"],
+]);
+
+function supportAliasRedirect(request) {
+  const url = new URL(request.url);
+  const targetPath = SUPPORT_ALIASES.get(url.pathname);
+  if (!targetPath) {
+    return null;
+  }
+
+  url.pathname = targetPath;
+  url.search = "";
+  url.hash = "";
+  return Response.redirect(url.toString(), 301);
+}
+
 export async function onRequest(context) {
+  const aliasResponse = supportAliasRedirect(context.request);
+  if (aliasResponse) {
+    return withFunctionSecurityHeaders(aliasResponse);
+  }
+
   const response = await context.next();
   return withFunctionSecurityHeaders(response);
 }
